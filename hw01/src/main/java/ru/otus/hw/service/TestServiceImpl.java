0d0e82cc409a6b8b476d5987589cc26b459aa290
Alexey Okhrimenko <alexey_nats@mail.ru>
2025-10-08 09:16:10 +0300

    working ver.1

--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,58 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
+import java.util.Scanner;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
+    private final QuestionDao questionDao;
 
     @Override
     public void executeTest() {
         ioService.printLine("");
-        ioService.printFormattedLine("Please answer the questions below%n");
-        // Получить вопросы из дао и вывести их с вариантами ответов
+        ioService.printFormattedLine("Please answer the questions below:");
+        var questions = questionDao.findAll();
+        int score = 0;
+        for (var q : questions) {
+            if (askQuestion(q)) {
+                score++;
+            }
+        }
+        ioService.printLine("Your score is " + score + " of " + questions.size());
+    }
+
+    private boolean askQuestion(Question question) {
+        ioService.printLine("");
+        ioService.printFormattedLine(question.text());
+        return processAnswers(question);
+
+    }
+
+    private boolean processAnswers(Question question) {
+        int correctAnswerNumber = 0;
+        for (int i = 0; i < question.answers()
+                                    .size(); i++) {
+            ioService.printLine((i + 1) + ". " + question.answers()
+                                                         .get(i)
+                                                         .text());
+            if (question.answers()
+                        .get(i)
+                        .isCorrect()) {
+                correctAnswerNumber = i+1;
+            }
+        }
+        ioService.printLine("");
+        ioService.printFormattedLine("Enter answer number:");
+        return correctAnswerNumber == getAnswerNumber();
+    }
+
+    private int getAnswerNumber() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+
     }
 }
