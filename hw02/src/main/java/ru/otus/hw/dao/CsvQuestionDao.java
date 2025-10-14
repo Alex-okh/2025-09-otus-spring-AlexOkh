5bcd9e2af0f111e4e36fe526abfc8f0f038ab495
Alexey Okhrimenko <alexey_nats@mail.ru>
2025-10-14 11:02:55 +0300

    Basic functionality.

--- hw02/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw02/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,23 +1,45 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Repository;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
     private final TestFileNameProvider fileNameProvider;
 
+    private final ResourceLoader resourceLoader;
+
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
-
-        return new ArrayList<>();
+        Resource resource = resourceLoader.getResource(fileNameProvider.getTestFileName());
+        try (Reader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
+            CsvToBean<QuestionDto> csvToBean =
+                    new CsvToBeanBuilder<QuestionDto>(reader).withType(QuestionDto.class)
+                                                             .withSeparator(';')
+                                                             .withSkipLines(1)
+                                                             .withIgnoreLeadingWhiteSpace(true)
+                                                             .build();
+            return csvToBean.stream()
+                            .map(QuestionDto::toDomainObject)
+                            .toList();
+        } catch (IOException e) {
+            throw new QuestionReadException("Could not read file", e);
+        }
     }
 }
--- hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,10 +1,13 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
+@Service
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
@@ -20,9 +23,35 @@ public class TestServiceImpl implements TestService {
         var testResult = new TestResult(student);
 
         for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+            var isAnswerValid = askQuestion(question); // Задать вопрос, получить ответ
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
+    }
+
+    private boolean askQuestion(Question question) {
+        ioService.printLine("");
+        ioService.printFormattedLine(question.text());
+        return processAnswers(question);
+
+    }
+
+    private boolean processAnswers(Question question) {
+        int correctAnswerNumber = 0;
+        for (int i = 0; i < question.answers()
+                                    .size(); i++) {
+            var answer = question.answers().get(i);
+            ioService.printLine((i + 1) + ". " + answer.text());
+            if (answer.isCorrect()) {
+                correctAnswerNumber = i + 1;
+            }
+        }
+        ioService.printLine("");
+        ioService.printFormattedLine("Enter answer number:");
+        return correctAnswerNumber == getAnswerNumber();
+    }
+
+    private int getAnswerNumber() {
+        return ioService.readIntForRange(1,5,"No such an option");
     }
 }
