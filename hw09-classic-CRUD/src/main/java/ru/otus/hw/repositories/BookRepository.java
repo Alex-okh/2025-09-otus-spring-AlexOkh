afe81af091a3fa1bc89e1b0270c24954be0033b7
Alexey Okhrimenko <alexey_nats@mail.ru>
2025-12-06 20:10:54 +0300

    Исправлен entityGraph

--- hw09-classic-CRUD/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw09-classic-CRUD/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -7,11 +7,11 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    @EntityGraph("book.html-authors-genres")
+    @EntityGraph("book-authors-genres")
     @Override
     Optional<Book> findById(Long id);
 
-    @EntityGraph("book.html-authors")
+    @EntityGraph("book-authors")
     List<Book> findAll();
 
 }
