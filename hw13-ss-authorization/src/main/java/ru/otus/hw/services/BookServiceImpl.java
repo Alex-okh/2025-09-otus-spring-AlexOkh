a5aecc2675338b24bf6799a7a391b0e5315c8f40
Alexey Okhrimenko <alexey_nats@mail.ru>
2026-01-25 22:21:17 +0300

    удален @PreAuthorize над Delete

--- hw13-ss-authorization/src/main/java/ru/otus/hw/services/BookServiceImpl.java
+++ hw13-ss-authorization/src/main/java/ru/otus/hw/services/BookServiceImpl.java
@@ -2,7 +2,6 @@ package ru.otus.hw.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
