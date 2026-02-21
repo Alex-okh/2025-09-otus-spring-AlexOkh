4d3b712f5763cbb6a0855824f3b3d575cf9222e6
Alexey Okhrimenko <alexey_nats@mail.ru>
2026-02-21 14:33:48 +0300

    Init hw15

---
+++ hw15-integration/src/test/java/ru/globus/hw15integration/Hw15IntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.globus.hw15integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw15IntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
